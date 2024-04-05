fd2ced5fa2e878efd5adc7beffb3ca2dc0d91e34
Adam Levoy <ajlevoy@gmail.com>
2024-04-05 08:11:13 -0700

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), react()],
+  output: "server",
+  adapter: netlify()
 });
