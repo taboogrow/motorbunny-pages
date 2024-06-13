bf49c90724f89d06e7980353cd63552a7e061c25
stormynight9 <ferjani.nader@hotmail.fr>
2024-06-13 23:06:58 +0100

    feat: Add Partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import netlify from '@astrojs/netlify';
-
 import compress from 'astro-compress';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
 			applyBaseStyles: false
 		}),
 		icon(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push']
+			}
+		}),
 		compress({
 			CSS: false
 		})
