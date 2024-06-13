26c02f2580c9f841ac1cc1e3c9cccf6ae0fac85b
stormynight9 <ferjani.nader@hotmail.fr>
2024-06-13 10:42:46 +0100

    optimized images , and add Astro compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-
 import netlify from '@astrojs/netlify';
+
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,10 @@ export default defineConfig({
 		tailwind({
 			applyBaseStyles: false
 		}),
-		icon()
+		icon(),
+		compress({
+			CSS: false
+		})
 	],
 	output: 'hybrid',
 	adapter: netlify()
