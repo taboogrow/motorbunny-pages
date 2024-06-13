2fe99f9539665a8d1dfc2da287727c1da68744cb
stormynight9 <ferjani.nader@hotmail.fr>
2024-06-13 09:40:46 +0100

    added custom properties to subscribed profiles

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -6,7 +6,7 @@ export const POST: APIRoute = async ({ request }) => {
 	const data = await request.formData();
 	const email = data.get('email') as string;
 
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 	if (!emailRegex.test(email)) {
 		return new Response(
 			JSON.stringify({
@@ -21,7 +21,9 @@ export const POST: APIRoute = async ({ request }) => {
 	const KLAVIYO_API_KEY = import.meta.env.KLAVIYO_API_KEY;
 	const KLAVIYO_LIST_ID = import.meta.env.KLAVIYO_LIST_ID;
 
-	const response = await fetch('https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs/', {
+	let profileId = null;
+
+	const createProfileResponse = await fetch('https://a.klaviyo.com/api/profiles/', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -30,36 +32,76 @@ export const POST: APIRoute = async ({ request }) => {
 		},
 		body: JSON.stringify({
 			data: {
-				type: 'profile-subscription-bulk-create-job',
+				type: 'profile',
 				attributes: {
-					profiles: {
-						data: [
-							{
-								type: 'profile',
-								attributes: {
-									email
-								},
-								properties: {
-									$source: 'Win A Motorbunny'
-								}
-							}
-						]
-					}
-				},
-				relationships: {
-					list: {
-						data: {
-							type: 'list',
-							id: KLAVIYO_LIST_ID
-						}
+					email,
+					properties: {
+						$source: 'Win A Motorbunny'
 					}
 				}
 			}
 		})
 	});
 
-	// if response is not accepted
-	if (response.status !== 202) {
+	if (createProfileResponse.status === 400) {
+		console.log('Invalid email');
+		return new Response(
+			JSON.stringify({
+				message: 'Invalid email'
+			}),
+			{
+				status: 400
+			}
+		);
+	}
+
+	// profile already exists with that email but we don't know if it's in the list, so we get the id from the error message
+	if (createProfileResponse.status === 409) {
+		console.log('Profile already exists');
+		profileId = (await createProfileResponse.json()).errors[0].meta.duplicate_profile_id;
+	}
+
+	if (createProfileResponse.status === 201) {
+		profileId = (await createProfileResponse.json()).data.id;
+	}
+
+	if (
+		(createProfileResponse.status !== 201 && createProfileResponse.status !== 409) ||
+		!profileId
+	) {
+		return new Response(
+			JSON.stringify({
+				message: 'Error creating profile'
+			}),
+			{
+				status: 500
+			}
+		);
+	}
+
+	console.log(profileId);
+
+	const addProfileToListResponse = await fetch(
+		`https://a.klaviyo.com/api/lists/${KLAVIYO_LIST_ID}/relationships/profiles/`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Klaviyo-API-Key ${KLAVIYO_API_KEY}`,
+				revision: '2024-05-15'
+			},
+			body: JSON.stringify({
+				data: [
+					{
+						type: 'profile',
+						id: profileId
+					}
+				]
+			})
+		}
+	);
+
+	if (addProfileToListResponse.status !== 204) {
 		return new Response(
 			JSON.stringify({
 				message: 'Error subscribing'
